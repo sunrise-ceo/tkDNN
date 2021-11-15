ef2afa22e38fd46c3bf415804ed1634ee36ee737
Artem Krivosheev <newyorktheusa@gmail.com>
2021-11-15 01:00:51 +0100

    Added Yolo2Detection header file.

---
+++ include/tkDNN/Yolo2Detection.h
@@ -0,0 +1,35 @@
+#ifndef Yolo2Detection_H
+#define Yolo2Detection_H
+#include <opencv2/videoio.hpp>
+#include "opencv2/opencv.hpp"
+
+#include "DetectionNN.h"
+
+namespace tk { namespace dnn { 
+
+class Yolo2Detection : public DetectionNN {
+private:
+    int num = 0;
+    int coords = 0;
+    dnnType* bias_h;
+    tk::dnn::RegionInterpret* region_interpret;
+    cv::Mat bgr_h;
+     
+public:
+
+    Yolo2Detection() {};
+    ~Yolo2Detection() {}; 
+
+    tk::dnn::dataDim_t idim;
+
+    bool init(const std::string& tensor_path, const int n_classes=80, const int n_batches=1, const float conf_thresh=0.3);
+    void preprocess(cv::Mat &frame, const int bi=0);
+    void postprocess(const int bi=0, const bool mAP=false);
+    void draw(std::vector<cv::Mat>& frames) ;
+};
+
+
+} // namespace dnn
+} // namespace tk
+
+#endif /* Yolo2Detection_H*/
